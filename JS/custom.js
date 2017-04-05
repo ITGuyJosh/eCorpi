4cec536bbe8fbd8b6b8992ffa7801cf248114a3f
itguyjosh <itguyjosh@outlook.com>
2017-04-05 14:07:37 +0100

    Added hotjar tracking

--- JS/custom.js
+++ JS/custom.js
@@ -10,6 +10,19 @@ var docName;
 
 //ON DOC READY
 $(document).ready(function() {
+
+  //TRACKING FOR TESTING
+  //Hotjar Tracking Code for https://1525770.linux.studentwebserver.co.uk/eCorp
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:466433,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+
+
     // notifications
     $("#positive-alert").hide();
     $("#negative-alert").hide();
